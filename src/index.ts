0aa39e6c7bc45286b1000382ca66e10377b53ec1
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-12-30 19:02:52 +0100

    run projen

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { ConstructLibrary, ConstructLibraryOptions } from "projen/lib/cdk";
+import { ConstructLibrary, ConstructLibraryOptions } from 'projen/lib/cdk';
 
 export class CDKTFConstruct extends ConstructLibrary {
   constructor(options: ConstructLibraryOptions) {
     super(options);
 
-    const cdktfVersion = "^0.8.3";
-    const constructVersion = "^10.0.12";
+    const cdktfVersion = '^0.8.3';
+    const constructVersion = '^10.0.12';
 
     this.addPeerDeps(`constructs@${constructVersion}`, `cdktf@${cdktfVersion}`);
-    this.addKeywords("cdktf");
+    this.addKeywords('cdktf');
   }
 }
 
