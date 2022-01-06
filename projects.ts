43df0bf50a53eeabcaa101ae9ade30983d9bb6d4
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-01-06 11:55:31 +0100

    add secrets to build package

--- projects.ts
+++ projects.ts
@@ -3,4 +3,5 @@ export type Project = { name: string };
 export const projects: Project[] = [
   { name: "cdktf-construct-base" },
   { name: "cdktf-local-exec" },
+  { name: "cdktf-local-build" },
 ];
