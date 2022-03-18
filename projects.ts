ee10d5a14cb91e8cc2011e458bdf9d5852c2b749
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-03-18 14:50:36 +0100

    chore: share other usable product with the team

--- projects.ts
+++ projects.ts
@@ -1,13 +1,14 @@
 export type Project = { name: string; shareWithUsers?: string[] };
 
+const cdktfTeam = ["ansgarm", "skorfmann", "xiehan", "schersh"];
 export const projects: Project[] = [
   { name: "cdktf-construct-base" },
   { name: "cdktf-local-exec" },
   { name: "cdktf-local-build" },
-  { name: "cdktf-cdk8s" },
-  { name: "cdktf-multi-stack-tfe" },
+  { name: "cdktf-cdk8s", shareWithUsers: cdktfTeam },
+  { name: "cdktf-multi-stack-tfe", shareWithUsers: cdktfTeam },
   {
     name: "cdktf-tf-module-stack",
-    shareWithUsers: ["ansgarm", "skorfmann", "xiehan", "schersh"],
+    shareWithUsers: cdktfTeam,
   },
 ];
